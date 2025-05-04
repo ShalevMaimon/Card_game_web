0681e0d4b97b48a4ef945fec525e919754cf9b47
Shalev <maimon.9999@gmail.com>
2025-05-04 16:04:11 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -6,6 +6,10 @@ function App() {
   const [player2Name, setPlayer2Name] = useState('');
   const [isGameStarted, setIsGameStarted] = useState(false);
 
+  const updateIsGameStarted = (newValue) => {
+    setIsGameStarted(newValue);
+  }; 
+
   return (
     <div className="App">
       {!isGameStarted ? (
@@ -36,66 +40,62 @@ function App() {
           </button>
         </div>
       ) : (
-        <GameScreen player1Name={player1Name} player2Name={player2Name} />
+        <GameScreen player1Name={player1Name} player2Name={player2Name} isGameStarted={isGameStarted} updateIsGameStarted={updateIsGameStarted} />
       )}
     </div>
   );
 }
 
-function GameScreen({ player1Name, player2Name }) {
-  const [player1Deck, setPlayer1Deck] = useState(Array.from({ length: 10 }, (_, i) => i + 1));
-  const [player2Deck, setPlayer2Deck] = useState(Array.from({ length: 10 }, (_, i) => i + 1));
+function GameScreen({ player1Name, player2Name,isGameStarted, updateIsGameStarted}) {
+  const numOfCards=6;
+  const allNumbers = Array.from({ length: 2 * numOfCards }, (_, index) => index + 1);
+  for (let i = allNumbers.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [allNumbers[i], allNumbers[j]] = [allNumbers[j], allNumbers[i]];
+  }
+  const firstList = allNumbers.slice(0, numOfCards);
+  const secondList = allNumbers.slice(numOfCards);
+  const [player1Deck, setPlayer1Deck] = useState(firstList);
+  const [player2Deck, setPlayer2Deck] = useState(secondList);
   const [isPlayer1Turn, setIsPlayer1Turn] = useState(true);
   const [roundWinner, setRoundWinner] = useState('');
   const [gameWinner, setGameWinner] = useState('');
   const [player1Card, setplayer1Card] = useState('');
   const [player2Card, setplayer2Card] = useState('');
   const [index1, setindex1] =  useState(0);
-  const [index2, setindex2] = useState(0);
+
 
   const drawCard1 = () => {
     let x=Math.floor(Math.random() * player1Deck.length);
-    let valuex=player2Deck[x];
+    let valuex=player1Deck[x];
     setindex1(x);
     setplayer1Card(valuex);
     setIsPlayer1Turn(!isPlayer1Turn);
   };
 
   const drawCard2 = () => {
-    if (player1Deck.length === 0 || player2Deck.length === 0) {
-      determineGameWinner();
-      return;
-    }
     let y=Math.floor(Math.random() * player2Deck.length);
     let valuey=player2Deck[y];
-    setindex2(y);
     setplayer2Card(valuey);
     win(valuey,y);
-
-    
   };
 
-  const win = (y,indexy) => {
-    if (player1Card > y) {
+  const win = (valuey,indexy) => {
+    if (player1Card > valuey) {
       setRoundWinner(player1Name);
-      const newDeck1 = [...player2Deck];
-      newDeck1.splice(indexy, 1); 
-      setPlayer2Deck(newDeck1);
-      setPlayer1Deck(player1Deck => [...player1Deck, y]);
-    } else if (y > player1Card) {
+      let newDeck2 = [...player2Deck];
+      newDeck2.splice(indexy, 1); 
+      setPlayer2Deck(newDeck2);
+      setPlayer1Deck(player1Deck => [...player1Deck, valuey]);
+    } else if (valuey > player1Card) {
       setRoundWinner(player2Name);
-      const newDeck1 = [...player1Deck];
+      let newDeck1 = [...player1Deck];
       newDeck1.splice(index1, 1); 
       setPlayer1Deck(newDeck1);
       setPlayer2Deck(player2Deck => [...player2Deck, player1Card]);
     } else {
       setRoundWinner('Tie');
     }
-
-    if (player1Deck.length === 0 || player2Deck.length === 0) {
-      determineGameWinner();
-    }
-
   };
   
 
@@ -104,6 +104,9 @@ function GameScreen({ player1Name, player2Name }) {
     setplayer1Card('');
     setplayer2Card('');
     setIsPlayer1Turn(true);
+    if (player1Deck.length === 0 || player2Deck.length === 0) {
+      determineGameWinner();
+    }
   };
 
   const determineGameWinner = () => {
@@ -115,11 +118,22 @@ function GameScreen({ player1Name, player2Name }) {
   };
 
   const resetGame = () => {
-    setPlayer1Deck(Array.from({ length: 10 }, (_, i) => i + 1));
-    setPlayer2Deck(Array.from({ length: 10 }, (_, i) => i + 1));
+    const allNumbers = Array.from({ length: 2 * numOfCards }, (_, index) => index + 1);
+    console.log(allNumbers);
+    for (let i = allNumbers.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      [allNumbers[i], allNumbers[j]] = [allNumbers[j], allNumbers[i]];
+    }
+    const firstList = allNumbers.slice(0, numOfCards);
+    const secondList = allNumbers.slice(numOfCards);
+    setPlayer1Deck(firstList);
+    setPlayer2Deck(secondList);
     setIsPlayer1Turn(true);
     setRoundWinner('');
     setGameWinner('');
+    setplayer1Card('');
+    setplayer2Card('');
+    updateIsGameStarted(false);
   };
 
   return (
@@ -130,7 +144,7 @@ function GameScreen({ player1Name, player2Name }) {
           <h2>{player1Name}</h2>
           <p>The number is:</p> {player1Card}
           <br></br>
-          <button onClick={drawCard1} disabled={!isPlayer1Turn || roundWinner}>
+          <button onClick={drawCard1} disabled={!isPlayer1Turn || roundWinner || gameWinner}>
            take a card
           </button>
           <br></br><br></br>
@@ -140,7 +154,7 @@ function GameScreen({ player1Name, player2Name }) {
           <h2>{player2Name}</h2>
           <p>The number is:</p> {player2Card}
           <br></br>
-          <button onClick={drawCard2} disabled={isPlayer1Turn || roundWinner}>
+          <button onClick={drawCard2} disabled={isPlayer1Turn || roundWinner || gameWinner}>
           take a card
           </button>
           <br></br><br></br>
@@ -153,7 +167,8 @@ function GameScreen({ player1Name, player2Name }) {
       </button>
       {gameWinner && (
         <div>
-          <p className="game-winner">Game Winner: {gameWinner}</p>
+          <br></br>
+          <h className="game-winner">Game Winner: {gameWinner} !!!!!</h><br></br>
           <button className="play-again-button" onClick={resetGame}>
             Play Again
           </button>
